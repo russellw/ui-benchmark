555f893e62e2efad52f0016245f20b220c5f096e
Russell Wallace <russell.wallace@gmail.com>
2024-05-15 11:17:57 +0100

    WPF

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,24 +1,56 @@
-﻿using System.Text;
+﻿using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace TypingAssist
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Stopwatch stopwatch;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Clear();
+            stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < 10; i++)
+            {
+                foreach (char letter in "abcdefghijklmnopqrstuvwxyz")
+                {
+                    textBox.AppendText(letter + " ");
+                    textBox.Focus();
+                    textBox.CaretIndex = textBox.Text.Length;
+                    Application.Current.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Background, new Action(delegate { }));
+                    ShowSuggestionBox();
+                }
+            }
+
+            stopwatch.Stop();
+            resultLabel.Content = $"Total time taken: {stopwatch.Elapsed.TotalSeconds:F4} seconds";
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            ShowSuggestionBox();
+        }
+
+        private void ShowSuggestionBox()
+        {
+            if (textBox.IsFocused)
+            {
+                int textLength = textBox.Text.Length;
+                Rect rect = textBox.GetRectFromCharacterIndex(textLength, true);
+
+                suggestionBox.Margin = new Thickness(rect.Right + 20, rect.Top + 10, 0, 0);
+                suggestionBox.Visibility = Visibility.Visible;
+            }
         }
     }
 }
